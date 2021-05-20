74c5992a283b077a97006fddced2e3a85e926646
TeoPapa <62250574+TeoPapa@users.noreply.github.com>
2021-05-20 13:04:22 +0300

    Changed some things
    Change the type of the variables for boy and girl, added the Start function and added the Animators

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,32 +5,48 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    public bool CharacterType; //If the character is boy it's true and if character is girl it's false.
+    public int CharacterType = 1; //If the character is boy it's 1 and if character is girl it's 2.
     public bool PlayingGame; //If the game is being played it's true, otherwise it's false.
     public bool OpenedInventory; //If the inventory is opened the value is true. When it's closed the value changes to false.
     public float PlayerMovementSpeed = 5f; //The speed that the player moves arround.
     public Rigidbody2D PlRB; //The body that player has, that allows us to use physics on him.
     public Vector2 Movement; //The movement input that changes the position of the player.
-    //public bool Story StoryLine; //The item that holds the whole storyline
+    //public Story StoryLine; //The item that holds the whole storyline
     public KeyCode InteractKey; //The key used for interactions.
     public Animator PlayerAnimator; //The animations used for the player's movements.
 
 
-    // Start is called before the first frame update
-    /*void Start()
+    void Start()
     {
-        
-    } */
+        CharacterType = PlayerPrefs.GetInt("CharChoice"); //fetch the player's choice of character
+        if(CharacterType == 2 && this.gameObject.tag == "PlayerM")
+        {
+            //if im a boy and player chose girl, i delete myself
+            Destroy(this.gameObject);
+        }
+        if(CharacterType == 1 && this.gameObject.tag == "PlayerF")
+        {
+            //if im a girl and player chose boy, i delete myself
+            Destroy(this.gameObject);
+        }
+    }
 
-    // Update is called once per frame
     void Update()
     {
         //Player's Movement
         Movement.x = Input.GetAxisRaw("Horizontal"); //A, D, < or >
-        Movement.y = Input.GetAxisRaw("Vertical"); //W, S, ^, !^
-
-        //TODO Here will be the animator's sets
-        //Animator's Sets
+        Movement.y = Input.GetAxisRaw("Vertical"); //W, S, ^, v
+                        
+        //Animations
+        PlayerAnimator.SetFloat("Horizontal", Movement.x);
+        PlayerAnimator.SetFloat("Vertical", Movement.y);
+        PlayerAnimator.SetFloat("Speed", Movement.sqrMagnitude);
+        
+        //Idles
+        if(Movement.x == 1 || Movement.x == -1 || Movement.y == 1 || Movement.y == -1) {
+            PlayerAnimator.SetFloat("LastMoveX", Movement.x);
+            PlayerAnimator.SetFloat("LastMoveY", Movement.y);
+        }
     }
 
     private void FixedUpdate()
